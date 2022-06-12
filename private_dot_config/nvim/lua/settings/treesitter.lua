28950dffc1ed79903655c48547e9ebec68319dd5
Sergey Koksharov <gentoosiastic@gmail.com>
2022-06-12 10:37:38 +0300

    fix: disable unstable treesitter highlighting for HTML in Neovim

--- private_dot_config/nvim/lua/settings/treesitter.lua
+++ private_dot_config/nvim/lua/settings/treesitter.lua
@@ -5,7 +5,7 @@ require'nvim-treesitter.configs'.setup {
 'json5', 'jsonc', 'llvm', 'lua', 'make', 'org', 'python', 'regex', 'rust', 'scss', 'toml', 'typescript', 'vim', 'yaml' },
 	highlight = {
 		enable = true,
-        disable = { 'org' },
+        disable = { 'html', 'org' },
         additional_vim_regex_highlighting = { 'org' }  -- Required since TS highlighter doesn't support all syntax features (conceal)
 	}
 }
